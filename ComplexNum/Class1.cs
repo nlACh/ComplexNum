45bf59d91f5043ca4556fe18a25f2fd5906fb9fa
Nilayan Paul <nlpl931@gmail.com>
2017-08-11 22:20:07 +0530

    a library to expand support for complex numbers in projects

--- ComplexNum/Class1.cs
+++ ComplexNum/Class1.cs
@@ -8,5 +8,50 @@ namespace ComplexNum
 {
     public class Class1
     {
+        public struct Complex
+        {
+            public Int32 re;
+            public Int32 im;
+        }
+        //method to add complex numbers
+        public Complex Add(Complex c1, Complex c2)
+        {
+            Complex c;
+            c.re = c1.re + c2.re;
+            c.im = c1.im + c2.im;
+            return c;
+        }
+        //method to subtract complex numbers
+        public Complex Sub(Complex c1, Complex c2)
+        {
+            Complex c;
+            c.re = c1.re - c2.re;
+            c.im = c1.im - c2.im;
+            return c;
+        }
+        //method to multiply complex numbers
+        public Complex Mul(Complex c1, Complex c2)
+        {
+            Complex c;
+            c.re = c1.re * c2.re - c1.im * c2.im;
+            c.im = c1.re * c2.im + c1.im + c2.re;
+            return c;
+        }
+        //method to "divide" complex numbers
+        public Complex Div(Complex c1, Complex c2)
+        {
+            Complex c;
+            //basically divison works by multiplying with conjugate...
+            c2.im = -c2.im;
+            c = Mul(c1, c2);
+            //so divide both components with c2.re^2+c2.im^2
+            c.re = c.re / (c2.re ^ 2 + c2.im ^ 2);
+            c.im = c.im / (c2.re ^ 2 + c2.im ^ 2);
+            return c;
+        }
+        public float Mod(Complex c)
+        {
+            return (c.re ^ 2 + c.im ^ 2);
+        }
     }
 }
